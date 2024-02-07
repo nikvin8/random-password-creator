eb61b606f7b6c8ce1ed41bdf36dad9b7bd9a45b4
Nikvin <150920532+nikvin8@users.noreply.github.com>
2024-02-07 15:38:12 +0100

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -2,9 +2,17 @@ from string import digits, ascii_letters
 import secrets
 
 
-def pass_gen():
-    return '-'.join([''.join([secrets.choice(digits + ascii_letters) for _ in range(8)]) for i in range(4)])
+# Let's create cryptographically strong random numbers. Using the module "secrets" allow us to avoid weakness of
+# pseudorandom number generator.
+def pass_gen(length=4):
+    return (secrets.choice(digits + ascii_letters + "!@#$%^&*~+=") for _ in range(length))
 
 
+# Generate a 16-character password separated by a dash every 4 characters.
 if __name__ == "__main__":
-    pass_gen()
+    password = ''
+    for i in range(4):
+        password += ''.join(pass_gen())
+        if i < 3:
+            password += '-'
+# Example output: "9xIU-O5g#-eGZ9-U348"
